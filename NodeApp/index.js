bccd67f19a1042c1a5236c7e13dce0fd0ebc4a0e
Omar <omher235@gmail.com>
2024-09-27 08:29:46 -0600

    correccion

---
+++ NodeApp/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+//const usuariosRutas = require("./rutas/rutasUsuarios");
+const productoRutas = require("./rutas/rutasProductos");
+const usuariosRutas = require("./rutas/rutasUsuarios");
+
+
+const app = express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use("/",usuariosRutas);
+app.use("/a",productoRutas);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log("Servidor en http://localhost:"+port)
+});
