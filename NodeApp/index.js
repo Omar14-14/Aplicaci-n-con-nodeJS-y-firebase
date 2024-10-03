c63c53773956b1c8c50aaad47ae3aa45a06b94d5
Omar <omher235@gmail.com>
2024-10-02 21:06:43 -0600

    Update index.js

--- NodeApp/index.js
+++ NodeApp/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 //const usuariosRutas = require("./rutas/rutasUsuarios");
 const productoRutas = require("./rutas/rutasProductos");
 const usuariosRutas = require("./rutas/rutasUsuarios");
+const ventasRutas = require("./rutas/rutasVentas");
 
 
 const app = express();
@@ -9,6 +10,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use("/",usuariosRutas);
 app.use("/a",productoRutas);
+app.use("/b",ventasRutas);
+
 
 const port = process.env.PORT || 3000;
 
